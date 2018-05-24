02538d01d96e6321af84d8311f5244a9d3c61f64
Mary-Kate James <marykatj@gmail.com>
2018-05-24 00:30:18 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |styles, languages|
-    languages.each do |language, data|
+    languages.collect do |language, data|
       data[:style] = [styles]
       new_hash = languages
       binding.pry
