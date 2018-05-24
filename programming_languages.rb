df0282ade2fb2d86396fee89ebd079ceba7206a6
Mary-Kate James <marykatj@gmail.com>
2018-05-24 00:55:26 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, languages|
+    languages.each do |language, data|
+      new_hash[language] ||= {}
+      new_hash[language][:type] = data[:type]
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << styles
+    end
+  end
+  new_hash
 end
+
+
+# need to add an if conditional to get JS to run
+# JS has two styles
