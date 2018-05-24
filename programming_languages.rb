be826b7a50f5975755d5bc6db71052827fd038fa
Mary-Kate James <marykatj@gmail.com>
2018-05-24 00:52:48 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,11 +4,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |styles, languages|
     languages.each do |language, data|
-      if new_hash[language]
-        new_hash[language][:style] << styles
-      else
-      new_hash[language] = {type: languages[:type], style: styles}
-    end
+      new_hash[language] = data
+      new_hash[language][:style] = []
+      new_hash[language][:style] << styles
     end
   end
   new_hash
